7ac27baa48ac172a33389ae3565b7d58f7aedfe3
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-03-23 15:05:47 +0300

    Update sht30_arduino.ino

--- SHT30/sht30_arduino/sht30_arduino.ino
+++ SHT30/sht30_arduino/sht30_arduino.ino
@@ -1,28 +1,28 @@
 #include <Wire.h>
 
-float ext_temp = 0.0; //ambient temp
-float ext_hum = 0.0; //ambient humidity
-
 void setup() {
- //init wire
- wire.begin();
- //init serial, baud 115200
- Serial.begin(115200);
- //print
- Serial.println("SHT30 sensor demo code");
+  //init wire
+  wire.begin();
+  //init serial, baud 115200
+  Serial.begin(115200);
+  //print
+  Serial.println("SHT30 sensor demo code");
 }
 
 void loop() {
   //get temp and humidity
   getExtTempHum();
+  //delay 500ms
+  delay(500);
 }
 
 inline void getExtTempHum() {
   uint8_t data[6] = { 0 };
   uint8_t config[2] = {0x2c, 0x06};
   uint8_t x = 0;
-
-  int temp = 0;
+  float ext_temp = 0.0; //ambient temp
+  float ext_hum = 0.0; //ambient humidity
+  flot temp = 0;
 
   Wire.beginTransmission(sht30_addr);
   Wire.write(config[0]);
@@ -38,4 +38,7 @@ inline void getExtTempHum() {
   temp = (data[0] * 256 + data[1]);
   ext_temp = double((-45.0 + (175.0 * (temp / 65535.0))) - 1.0);
   ext_hum = double(100.0 * ((data[3] * 256.0 + data[4]) / 65535.0));
+
+  //print the values
+  Serial.print("Temp: #.2f, Hum: %.2f\n", ext_temp, ext_hum);
 }
