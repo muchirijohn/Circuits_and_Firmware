3c16f87c3ba523f6682dac49049d83a261582867
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-03-30 16:10:57 +0300

    update

--- SHT30/sht30_stm32/Core/Src/i2c.c
+++ SHT30/sht30_stm32/Core/Src/i2c.c
@@ -87,7 +87,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
     */
     GPIO_InitStruct.Pin = SCL_Pin|SDA_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
