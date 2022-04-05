0ba510711f06296f27ea786859bd42a23f44ead6
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-04-05 15:37:34 +0300

    Update Firmware_LED_Acrylic_Arduino.ino

--- LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
+++ LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
@@ -33,6 +33,7 @@ void displaySensorDetails(void)
   delay(500);
 }
 
+
 //setup
 void setup() {
   Serial.begin(115200);
@@ -53,6 +54,9 @@ void setup() {
   displaySensorDetails();
 }
 
+uint8_t new_state = 0;
+uint8_t old_state = 0;
+uint8_t show_type = 0;
 //loop
 void loop() {
   for (int i = 0; i < NUMPIXELS; i++) {
@@ -69,6 +73,123 @@ void loop() {
   int y = accel.getY();
   /* Display some basic information on this sensor */
   Serial.printf("X: %d , Y: %d\n", x, y);
-  //Serial.print("X: "); Serial.print(door_stat); Serial.print("  ");
-  delay(500);
+
+  if(x =>150 && new_state != old_state){
+    show_type++;
+      if (show_type > 9)
+        show_type=0;
+      startShow(show_type);
+    new_state = old_state;
+  }
+  delay(100);
+}
+
+void startShow(int i) {
+  switch(i){
+    case 0: colorWipe(pixels.Color(0, 0, 0), 50);    // Black/off
+            break;
+    case 1: colorWipe(pixels.Color(255, 0, 0), 50);  // Red
+            break;
+    case 2: colorWipe(pixels.Color(0, 255, 0), 50);  // Green
+            break;
+    case 3: colorWipe(pixels.Color(0, 0, 255), 50);  // Blue
+            break;
+    case 4: theaterChase(pixels.Color(127, 127, 127), 50); // White
+            break;
+    case 5: theaterChase(pixels.Color(127,   0,   0), 50); // Red
+            break;
+    case 6: theaterChase(pixels.Color(  0,   0, 127), 50); // Blue
+            break;
+    case 7: rainbow(20);
+            break;
+    case 8: rainbowCycle(20);
+            break;
+    case 9: theaterChaseRainbow(50);
+            break;
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < pixels.numPixels(); i=i+3) {
+        pixels.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      pixels.show();
+
+      delay(wait);
+
+      for (int i=0; i < pixels.numPixels(); i=i+3) {
+        pixels.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < pixels.numPixels(); i=i+3) {
+        pixels.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      pixels.show();
+
+      delay(wait);
+
+      for (int i=0; i < pixels.numPixels(); i=i+3) {
+        pixels.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
