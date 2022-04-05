8510833ecedf20877337c5d8a91bd7f7ce9c30d6
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-04-05 15:15:45 +0300

    Create Firmware_LED_Acrylic_Arduino.ino

---
+++ LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
@@ -0,0 +1,30 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            13
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      2
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+void setup() {
+  // put your setup code here, to run once:
+pixels.begin();
+}
+
+void loop() {
+  for(int i=0;i<NUMPIXELS;i++){
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(500); // Delay for a period of time (in milliseconds).
+
+  }
+}
