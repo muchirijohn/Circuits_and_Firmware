2179dd83d13c847fe3412f324eb7443f7b474e7f
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-04-05 15:20:33 +0300

    Update Firmware_LED_Acrylic_Arduino.ino

--- LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
+++ LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
@@ -1,3 +1,4 @@
+#include <Wire.h>
 #include <Adafruit_ADXL345_U.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -10,13 +11,37 @@
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      2
-
+//accel unique id 
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+//init neopixels
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+//display sensor details
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+//setup
 void setup() {
   // put your setup code here, to run once:
 pixels.begin();
+accel.setRange(ADXL345_RANGE_16_G);
+
 }
 
+//loop
 void loop() {
   for(int i=0;i<NUMPIXELS;i++){
 
