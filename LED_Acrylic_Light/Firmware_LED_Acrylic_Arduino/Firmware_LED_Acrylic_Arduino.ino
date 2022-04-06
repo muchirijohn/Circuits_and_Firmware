3eef9a849e9e8e5d791b7476ae4cedcb9caac631
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-04-06 21:35:05 +0300

    Update Firmware_LED_Acrylic_Arduino.ino

--- LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
+++ LED_Acrylic_Light/Firmware_LED_Acrylic_Arduino/Firmware_LED_Acrylic_Arduino.ino
@@ -23,10 +23,10 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 //init neopixels
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-
 uint8_t new_state = 0;
 uint8_t old_state = 0;
 uint8_t show_type = 0;
+uint8_t rb_cycles = 0;
 
 //display sensor details
 void displaySensorDetails(void)
@@ -122,19 +122,6 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-uint8_t rb_cycles = 0;
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for (j = 0; j < 256; j++) {
-    for (i = 0; i < pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel((i + j) & 255));
-    }
-    pixels.show();
-  }
-  delay(wait);
-}
-
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
@@ -156,55 +143,4 @@ void rainbowCycle(uint8_t wait) {
     }
     delay(wait);
   }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
-    for (int q = 0; q < 3; q++) {
-      for (int i = 0; i < pixels.numPixels(); i = i + 3) {
-        pixels.setPixelColor(i + q, c);  //turn every third pixel on
-      }
-      pixels.show();
-
-      delay(wait);
-
-      for (int i = 0; i < pixels.numPixels(); i = i + 3) {
-        pixels.setPixelColor(i + q, 0);      //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
-    for (int q = 0; q < 3; q++) {
-      for (int i = 0; i < pixels.numPixels(); i = i + 3) {
-        pixels.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
-      }
-      pixels.show();
-
-      delay(wait);
-
-      for (int i = 0; i < pixels.numPixels(); i = i + 3) {
-        pixels.setPixelColor(i + q, 0);      //turn every third pixel off
-      }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
